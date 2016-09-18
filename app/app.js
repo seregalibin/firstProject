b6e675906dfc27687e560b998744cc03194a25d4
Sergey Libin <serega.libin@gmail.com>
2016-09-18 11:59:42 +0300

    constant

--- app/app.js
+++ app/app.js
@@ -1,86 +1,95 @@
 
 var app = angular.module('game', []);
-	app.controller('teamsController', ['$scope', function($scope){
-		$scope.team1 = '';
-		$scope.team2 = '';
+	
+	app.constant('standard', {
+		team1: '',
+		team2: '',
 
-		$scope.score1 = 0;
-		$scope.score2 = 0;
+		score1: 0,
+		score2: 0,
 
-		$scope.game = 162;
+		gamepoints: 162,
+		extraScore: 0,
+
+	 	addings: [
+			{name: 'Терц #1 (+30)', score:30},
+			{name: 'Терц #2 (+30)', score:30},
+			{name: 'Терц #3 (+30)', score:30},
+			{name: 'Терц #4 (+30)', score:30},
+			{name: 'Полтина #1 (+50)', score:50},
+			{name: 'Полтина #2 (+50)', score:50},
+			{name: 'Полтина #3 (+50)', score:50},
+			{name: 'Белла(+20)', score:20}
+			]
+
+	});
+
+	  
+
+
+	app.controller('teamsController', ['$scope', 'standard', function($scope, standard){
+		$scope.team1 = standard.team1;
+		$scope.team2 = standard.team2
+
+		$scope.score1 = standard.score1;
+		$scope.score2 = standard.score2;
+
+		$scope.gamepoints = standard.gamepoints
+
 		$scope.extraScore = 0;
 
-		$scope.actionList = [{actionTeam1: 0,actionTeam2: 0, delete:false}];
+		$scope.actionList = [];
 
-/////   Счетчик для 1 варианта   /////
 
-		$scope.addScore = function(){
-			
-			$scope.score1 = $scope.score1 + $scope.addTo1 *1;
-			$scope.score2 = $scope.score2 + $scope.addTo2 *1;
+			$scope.addScore21 = function(){
 
-			$scope.actionList.push({actionTeam1:$scope.addTo1, actionTeam2:$scope.addTo2, delete:false});
+				var countPointsTeam11 = $scope.extraScore + $scope.addTo21;
+				var countPointsTeam12 = $scope.gamepoints + $scope.extraScore - countPointsTeam11;
 
-			$scope.addTo1 = "";
-			$scope.addTo2 = "";
+				$scope.actionList.push({actionTeam1:countPointsTeam11, actionTeam2:countPointsTeam12, delete:false});
 
-			$scope.checkJack = checked;
-		};
+				$scope.addTo21 = "";
 
-/////   Счетчик для 2 варианта ////
-		$scope.addScore21 = function(){
+				$scope.extraScore = 0;
 
-			$scope.countPointsTeam2 = $scope.game + $scope.extraScore - $scope.addTo21;
+				$scope.selected = [];
 
-			$scope.actionList.push({actionTeam1:$scope.addTo21, actionTeam2:$scope.countPointsTeam2, delete:false});
+			};
 
-			$scope.addTo21 = "";
+			$scope.addScore22 = function(){
 
-			$scope.extraScore = 0;
+				var countPointsTeam21 = $scope.gamepoints + $scope.extraScore - $scope.addTo21;
+				var countPointsTeam22 = $scope.extraScore + $scope.addTo21;
 
-			$scope.selected = [];
+				$scope.actionList.push({actionTeam1:countPointsTeam21, actionTeam2:countPointsTeam22, delete:false});
 
-		};
+				$scope.addTo21 = "";
 
-		$scope.addScore22 = function(){
+				$scope.extraScore = 0;
 
-			$scope.countPointsTeam1 = $scope.game + $scope.extraScore - $scope.addTo21;
+				$scope.selected = [];
+			};
 
-			$scope.actionList.push({actionTeam1:$scope.countPointsTeam1, actionTeam2:$scope.addTo21, delete:false});
+			$scope.addScore3 = function(team){
+	 			angular.forEach($scope.selected, function(x){
+	 				if (x.score){
+	 					$scope.extraScore = $scope.extraScore + x.score;
+	 				}
+	 			});
 
-			$scope.addTo21 = "";
+				if (team == 1) 
+					$scope.addScore21()
+				 else 
+					$scope.addScore22();
+				
+			};
 
-			$scope.extraScore = 0;
 
-			$scope.selected = [];
-		};
-
-		$scope.addScore3 = function(team){
- 			angular.forEach($scope.selected, function(x){
- 				if (x.score){
- 					$scope.extraScore = $scope.extraScore + x.score;
- 				}
- 			});
-
-			if (team == 1) 
-				$scope.addScore21()
-			 else 
-				$scope.addScore22();
-			
-		};
 
 ///// Прибавление очков //////
 
-		  	$scope.addings = [
-				{name: 'Терц #1 (+30)', score:30},
-				{name: 'Терц #2 (+30)', score:30},
-				{name: 'Терц #3 (+30)', score:30},
-				{name: 'Терц #4 (+30)', score:30},
-				{name: 'Полтина #1 (+50)', score:50},
-				{name: 'Полтина #2 (+50)', score:50},
-				{name: 'Полтина #3 (+50)', score:50},
-				{name: 'Белла(+20)', score:20}
-				];
+		  	$scope.addings = standard.addings
+
 
 	        $scope.selected = [];
 
@@ -100,8 +109,8 @@ var app = angular.module('game', []);
         	   $scope.remove = function() {
 		        var oldList = $scope.actionList;
 		        $scope.actionList = [];
-		        $scope.score1=0;
-		        $scope.score2=0;
+		        $scope.score1 = 0;
+		        $scope.score2 = 0;
 		        angular.forEach(oldList, function(x) {
 		            if (!x.delete) {
 		            	$scope.actionList.push(x);
@@ -114,14 +123,16 @@ var app = angular.module('game', []);
 		  		};
 
 		    	$scope.startNewGame = function() {
-		    		$scope.team1 = '';
-					$scope.team2 = '';
+					$scope.team1 = standard.team1;
+					$scope.team2 = standard.team2
 
-		    		$scope.actionList = [{actionTeam1: 0,actionTeam2: 0, delete:false}];
-		         $scope.score1=0;
-		         $scope.score2=0;
+					$scope.score1 = standard.score1;
+					$scope.score2 = standard.score2;
+
+					$scope.extraScore = standard.extraScore;
+
+		    		$scope.actionList = [];
 
 		    	};
-
 
 	}]);
